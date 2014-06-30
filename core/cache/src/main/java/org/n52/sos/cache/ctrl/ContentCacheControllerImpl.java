61dcb21041466210e658ab13628b6d4a4cc366de
Christian Autermann <c.autermann@52north.org>
2014-06-30 11:23:03 +0200

    configurable write delay and non-blocking cache serialization

--- core/cache/src/main/java/org/n52/sos/cache/ctrl/ContentCacheControllerImpl.java
+++ core/cache/src/main/java/org/n52/sos/cache/ctrl/ContentCacheControllerImpl.java
@@ -37,8 +37,8 @@ import java.util.concurrent.locks.ReentrantLock;
 import org.n52.sos.cache.ContentCacheUpdate;
 import org.n52.sos.cache.WritableContentCache;
 import org.n52.sos.cache.ctrl.action.CompleteCacheUpdate;
-import org.n52.sos.cache.ctrl.persistence.AsyncCachePersistingStrategy;
 import org.n52.sos.cache.ctrl.persistence.CachePersistenceStrategy;
+import org.n52.sos.cache.ctrl.persistence.CachePersistenceStrategyFactory;
 import org.n52.sos.ogc.ows.OwsExceptionReport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -70,7 +70,7 @@ public class ContentCacheControllerImpl extends AbstractSchedulingContentCacheCo
     }
 
     public ContentCacheControllerImpl() {
-        this(new AsyncCachePersistingStrategy());
+        this(CachePersistenceStrategyFactory.getInstance().create());
     }
 
     private void loadOrCreateCache() {
